d639e6db93b52a4a37edcaa5c413f71d1aa231ac
ICRAR/Omar <00103780@uwa.edu.au>
2023-03-28 09:47:01 +0800

    Added Shel script

---
+++ Scripts/ICRAR_excer_Job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#SBATCH --job-name=ICRAR_excer_Job
+#SBATCH --partition=work
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:02:00
+
+echo "Starting program"
+
+cd /software/projects/pawsey0411/oanwar/2023-01-data-science-exercise/tests
+python3 exercise_functions_test.py
