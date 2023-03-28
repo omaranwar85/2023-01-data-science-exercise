61e0a025af283dced2e238eb7db8aa026740c0a7
ICRAR/Omar <00103780@uwa.edu.au>
2023-03-28 13:52:54 +0800

    Modified Env script

--- Scripts/InstallReq.sh
+++ Scripts/InstallReq.sh
@@ -1,21 +1,18 @@
 #!/bin/bash -l
-#SBATCH --job-name=CreateEnv
+#SBATCH --job-name=InstallReq
 #SBATCH --account=pawsey0411
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=00:01:00
 
-echo "Creating ENV"
-
-module load python/3.9.15
-python3 -m venv /software/projects/pawsey0411/oanwar/TestEnv
+echo "Installing Requirements"
 
 cd /software/projects/pawsey0411/oanwar
 
 source TestEnv/bin/activate
 pip install --upgrade pip
 
-cd /
+cd /CreateEnv.sh
 pip install -r requirements.txt
 
-echo "Done creating 'TestEnv' ENV"
+echo "Done installing the requirements"
