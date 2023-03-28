aca43a6ec5bd99d8ba2bc7e36bea69d9ad5431d8
ICRAR/Omar <00103780@uwa.edu.au>
2023-03-28 10:34:58 +0800

    Modified Env script

--- Scripts/CreateEnv.sh
+++ Scripts/CreateEnv.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -l
-#SBATCH --job-name=ICRAR_excer_Job
+#SBATCH --job-name=CreateEnv
 #SBATCH --partition=work
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=00:02:00
 
 echo "Creating ENV"
-python3 -m ICRAR_excer_Job /software/projects/pawsey0411/oanwar/
+python3 -m TestEnv /software/projects/pawsey0411/oanwar/
 
 cd /software/projects/pawsey0411/oanwar/2023-01-data-science-exercise
 
-source ICRAR_excer_Job/bin/activate
+source TestEnv/bin/activate
 pip install -r requirements.txt
 
-echo "Done creating 'ICRAR_excer_Job' ENV"
+echo "Done creating 'TestEnv' ENV"
