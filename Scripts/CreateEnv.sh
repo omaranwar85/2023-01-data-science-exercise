743426fdad54622fbbc8b1744c260cf3b21e432d
ICRAR/Omar <00103780@uwa.edu.au>
2023-03-28 10:56:43 +0800

    Modified Env script

--- Scripts/CreateEnv.sh
+++ Scripts/CreateEnv.sh
@@ -6,9 +6,11 @@
 #SBATCH --time=00:02:00
 
 echo "Creating ENV"
-python3 -m TestEnv /software/projects/pawsey0411/oanwar/
 
-cd /software/projects/pawsey0411/oanwar/2023-01-data-science-exercise
+module load python/3.9.15
+python3 -m venv /software/projects/pawsey0411/oanwar/TestEnv
+
+cd /software/projects/pawsey0411/oanwar
 
 source TestEnv/bin/activate
 pip install -r requirements.txt
