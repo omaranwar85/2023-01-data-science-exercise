f5ce94c3a53ef43903283065aff809b475abed0d
ICRAR/Omar <00103780@uwa.edu.au>
2023-03-29 10:39:41 +0800

    Added requirements shell

--- Scripts/ICRAR_excer_Job.sh
+++ Scripts/ICRAR_excer_Job.sh
@@ -27,7 +27,7 @@ echo "Modules loaded"
 echo "Generating clean files"
 cd $MYSCRATCH
 
-export PYTHONPATH=${/software/projects/pawsey0411/oanwar/2023-01-data-science-exercise/src
+export PYTHONPATH=${/software/projects/pawsey0411/oanwar/2023-01-data-science-exercise/src}
 python3 /software/projects/pawsey0411/oanwar/2023-01-data-science-exercise/tests/exercise_functions_test.py
 
 deactivate
