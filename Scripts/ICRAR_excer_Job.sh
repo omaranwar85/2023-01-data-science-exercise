02aa8405494b4e99154976662d9df88d2762b5c4
ICRAR/Omar <00103780@uwa.edu.au>
2023-03-29 10:37:54 +0800

    Added requirements shell

--- Scripts/ICRAR_excer_Job.sh
+++ Scripts/ICRAR_excer_Job.sh
@@ -5,11 +5,19 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --time=00:02:00
 
+echo "Creating ENV"
+
+module load python/3.9.15
+python3 -m venv /software/projects/pawsey0411/oanwar/TestEnv
+
+pip install --upgrade pip
+
+echo "Done creating 'TestEnv' ENV"
+
 source /software/projects/pawsey0411/oanwar/TestEnv/bin/activate
 echo "Environment activated"
 
 echo "Loading Modules"
-module load python/3.9.15
 module load numpy/1.21.5
 module load pandas/1.4.4
 module load python_dateutil/2.8.2
@@ -18,5 +26,8 @@ echo "Modules loaded"
 
 echo "Generating clean files"
 cd $MYSCRATCH
-add PYTHONPATH:/software/projects/pawsey0411/oanwar/2023-01-data-science-exercise/src
+
+export PYTHONPATH=${/software/projects/pawsey0411/oanwar/2023-01-data-science-exercise/src
 python3 /software/projects/pawsey0411/oanwar/2023-01-data-science-exercise/tests/exercise_functions_test.py
+
+deactivate
